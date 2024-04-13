4c4f6b282ed7d0d899c7890b529d4a8866416e9a
Haxalott's Developments <43390732+Haxalott@users.noreply.github.com>
2024-04-13 23:06:06 +1000

    Updated fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,17 +3,16 @@ game 'gta5'
 
 author 'Haxalott'
 description 'Rockstar Editor script'
-version '1.0.0'
+version '1.0.1'
 
 lua54 'yes'
 
 shared_scripts { 
-    '@ox_lib/init.lua', 
-    'locales.lua' 
+    '@ox_lib/init.lua'
 }
 
 client_script { 
-    'client.lua', 
+    'client.lua'
 }
 
 dependency { 
